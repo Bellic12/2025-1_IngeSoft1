1ae2b11005652907e35233db6c8623877a1218b2
DEIBYD SANTIAGO BARRAGAN GAITAN <116578796+DeibydBarragan@users.noreply.github.com>
2025-07-21 17:01:46 -0500

    Logo de la app añadido

--- Proyecto/pretty-exam/electron/main.js
+++ Proyecto/pretty-exam/electron/main.js
@@ -39,7 +39,7 @@ let win = null
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    icon: path.join(process.env.VITE_PUBLIC, 'Logo.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.mjs'),
     },
